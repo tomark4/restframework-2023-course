13185e39d3dc5150756dd431a68b8934cc1e735f
Jose Quintero <tomark4@gmail.com>
2023-02-07 10:16:51 -0400

    Create, update, delete cbv

--- project3-playground/pages/urls.py
+++ project3-playground/pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import PagesListView, PageDetailView
+from .views import PagesListView, PageDetailView, PageCreateView, PageEditView, PageDeleteView
 
-urlpatterns = [
-    path('', PagesListView.as_view(), name='pages'),
-    path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name='page'),
-]
+pages_patterns = ([
+    path('', PagesListView.as_view(), name='list'),
+    path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name='detail'),
+    path('create/', PageCreateView.as_view(), name='create'),
+    path('edit/<int:pk>/', PageEditView.as_view(), name='update'),
+    path('delete/<int:pk>/', PageDeleteView.as_view(), name='delete'),
+], 'pages')
--- project3-playground/pages/views.py
+++ project3-playground/pages/views.py
@@ -1,10 +1,38 @@
 from .models import Page
 from django.views.generic.list import ListView 
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-
+# listar
 class PagesListView(ListView):
     model = Page
 
+# detail
 class PageDetailView(DetailView):
     model = Page
+
+# create
+class PageCreateView(CreateView):
+    model = Page
+    fields = ('title','content', 'order')
+    success_url = reverse_lazy('pages:list')
+
+    # define success url with class method
+    # def get_success_url(self):
+    #     return reverse('pages:list')
+
+# update
+class PageEditView(UpdateView):
+    model = Page
+    fields = ('title','content','order')
+    template_name_suffix = "_update_form"
+    success_url = reverse_lazy('pages:list')
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('pages:update',args=[self.object.pk]) + '?ok'
+
+
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:list')
